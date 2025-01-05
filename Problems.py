af3988b8df18d19d2a0ebccba4826a1b967d8056
gurunathreddy2727@gmail.com <gurunathreddy2727@gmail.com>
2025-01-05 21:27:12 +0530

    fifth commit

--- Problems.py
+++ Problems.py
@@ -1,16 +1,154 @@
-#simple Interst PTR/100
-
-def simple_interse(p,t,r):
-    print("the princple is ", p)
-    print("the time is ", t)
-    print("the rate of interest is ", r)
-
-    si = (p * t * r)/100
-
-    print("the simple_interst is ", si)
-    return si
-
-P = int(input("enter the priciple amount "))
-T = int(input("enter the time period "))
-R = int(input("enter the rate of interest"))
-simple_interse(P,T,R)
+# #simple Interst PTR/100
+#
+# def simple_interse(p,t,r):
+#     print("the princple is ", p)
+#     print("the time is ", t)
+#     print("the rate of interest is ", r)
+#
+#     si = (p * t * r)/100
+#
+#     print("the simple_interst is ", si)
+#     return si
+#
+# P = int(input("enter the priciple amount "))
+# T = int(input("enter the time period "))
+# R = int(input("enter the rate of interest"))
+# simple_interse(P,T,R)
+#
+#
+# #sum of ARRAY
+#
+# def sum(arr):
+#     sum = 0
+#
+#     for i in arr:
+#         sum = sum + i
+#
+#         return (sum)
+# if __name__ == "__main__":
+#     arr = [12, 3, 4, 15]
+#     n = len(arr)
+#     ans = sum(arr)
+#     print("sum of the array is ", ans)
+#
+# BUBBLE SORT IN PYTHON
+#
+# def bubble_sort(arr):
+#      n = len(arr)
+#
+#      for i in range(n):
+#          for j in range(0, n-i-1):
+#              if arr[j]> arr[j+1]:
+#                  arr[j], arr[j+1] = arr[j+1], arr[j]
+#
+# if __name__ == "__main__":
+#     sample_list =[63,34,23,12,22,11,90]
+#
+#     print("original list:", sample_list)
+#
+#     bubble_sort(sample_list)
+#     print("sorted list:", sample_list)
+#
+# CALENDER
+# import calendar
+#
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+#
+# print(calendar.month(yy, mm))
+#
+#
+# NUMBER IS POSITIVE OR NEGATIVE
+#
+# def CheckNumber(n):
+#     if n>0:
+#         print("It is positive")
+#     elif n<0:
+#         print("It is Negative")
+#     else:
+#         print("give valid number not zero")
+#
+# n = int(input("Enter number: "))
+# CheckNumber(n)
+#
+# EVEN OR ODD
+# num = int(input("enter a Number: "))
+# if (num%2) == 0:
+#     print("the number is even")
+# else:
+#     print("the number is odd")
+#
+# CHECK THE NUMBER IS PRIME NUMBER
+#
+# def primecheck(a):
+#     if a>1:
+#         for i in range(2,int(a/2)+1):
+#             if(a%i) == 0:
+#                 print(a, "is not a prime number")
+#                 break
+#         else:
+#             print(a, "is a prime number")
+#     else:
+#         print(a, "is not a prime number")
+#
+# a = int(input("Enter an imput number: "))
+#
+# primecheck(a)
+# AREA OF TRIANGLE
+#
+# height = float(input("enter height: "))
+# base = float(input("enter base"))
+# area = (1/2) *base *height
+#
+# print("the area of the triange is ", area)
+#
+#
+# MILES TO KM 1mile is 1.6km
+#
+# km = float(input("enter km: "))
+# miles = km * 0.621371
+#
+# print(km, " km in miles ", miles)
+#
+#
+# LEAP year
+#
+# year = int(input("enter a year: "))
+#
+# if (year % 400 ==0) and (year % 100== 0):
+#     print(year, "is a learp year")
+# elif(year % 4 ==0 ) and (year % 100 != 0):
+#     print((year, " is a leap year"))
+# else:
+#    print(year, " is not a leap year")
+#
+# RANDOM NUMBER
+#
+# import random
+# print(random.randint(1,9))
+#
+# SWAP TWO NUMBER
+#
+# x = 5
+# y = 10
+#
+# temp = x
+# x = y
+# y = temp
+#
+# print('the value of x after swaping: {}'.format(x))
+# print('the value of x after swaping: {}'.format(y))
+#
+#
+# FIND THE LARGEST NUMBER
+# num1 = int(input("enter number1: "))
+# num2 = int(input("enter numebr2: "))
+# num3 = int(input("enter number3: "))
+#
+# if(num1>= num2) and (num1 >= num3):
+#     print(num1, " largest")
+# elif(num2 >= num1) and (num2>= num3):
+#     print(num2, " largest")
+# else:
+#     print(num3, " largest")
+#
